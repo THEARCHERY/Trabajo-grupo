13828eacaa7ddb066c6a1dfcfee6e9cb04a3158a
claudiamartinezurango <clauclicli1616@gmail.com>
2023-10-09 14:05:07 +0200

    Introduccion LadoLuminoso

---
+++ LadoLuminoso.py
@@ -0,0 +1,9 @@
+class LadoLuminoso:
+	def init(self,nombre,colorS,nave):
+		self.nombre = nombre
+		self.colorS = colorS
+		self.nave = nave
+
+	def mostrar(self):
+		print(f"El jedi {self.nombre} tiene el sable de color {self.colorS} y su nave es {self.nave}")
+
